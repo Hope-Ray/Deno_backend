0275909f138db938f2de4f1b75104e3bd0abff77
Anurag <anurag.shetty@salt-tech.com>
2024-12-20 18:31:05 +0530

    Json content added

---
+++ src/utils/json-content-required.ts
@@ -0,0 +1,21 @@
+import type { z } from "@hono/zod-openapi";
+
+// eslint-disable-next-line ts/ban-ts-comment
+export type ZodSchema =
+  | z.ZodUnion<[z.ZodTypeAny, ...z.ZodTypeAny[]]>
+  | z.AnyZodObject
+  | z.ZodArray<z.AnyZodObject>;
+
+import jsonContent from "./json-content.ts";
+
+const jsonContentRequired = <T extends ZodSchema>(
+  schema: T,
+  description: string
+) => {
+  return {
+    ...jsonContent(schema, description),
+    required: true,
+  };
+};
+
+export default jsonContentRequired;
